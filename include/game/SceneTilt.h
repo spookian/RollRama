0bc0f0c0fccc605a84161eac1c1e038c49ddc45d
spookian <ianbow111@gmail.com>
2023-12-18 13:05:56 -0500

    more setup

--- include/game/SceneTilt.h
+++ include/game/SceneTilt.h
@@ -1,29 +1,90 @@
 #define MAX_CLONES 20
 
-typedef struct Vector3_s
+namespace hel::math
 {
-	float x, y, z;
-} Vector3;
+	struct Vector3
+	{
+		float x, y, z;
+	}; // include functions later
+}
 
-
-typedef struct
+namespace mem
 {
-	unsigned long num;
-	IEntity *list[MAX_CLONES];
-} EntityList; // order will not and shall not matter.
+	class IAllocator // welcome to the dark side of c++ reverse engineering, finding a fucking allocator.
+	{
+		
+	};
 
-typedef struct SceneTilt_s
+	class Memory
+	{
+		IAllocator& sceneHeap();
+	};
+} // normally these class declarations would be problematic because who the fuck wants an empty class? but these are just here to act as convoluted void pointers
+
+namespace g3d
 {
-	void *vtable; // see my documentation on the scn::IScene class
+	
+	class RootContext
+	{
+		RootContext(mem::IAllocator& heap, unsigned long unk0, unsigned long unk1, unsigned long unk2, unsigned long unk3);
+	};
 
-	Vector3 cam_pos;
-	Vector3 cam_rot; // radians
+	class Root
+	{
+		Root(const RootContext& context);
+	};
 
-	IEntity *player;
-	EntityList clone_list;
-	EntityList enemy_list;
-} SceneTilt;
+	class Model
+	{
+		Model(); // wip definition
 
+		void RegisterToRoot(const Root& root);
+	};
+}
+
+namespace scn
+{
+	// haven't decomped yet
+	class DrawReqInfo
+	{
+	};
+
+	// interface
+	class IScene
+	{
+		public:
+		~IScene();
+		virtual void updateMain();
+		virtual void updateUseGPU();
+		virtual void updateDebug();
+		virtual void draw(const scn::DrawReqInfo& info);
+		virtual void isSceneEnd() const;
+	};
+}
+// move these eyesores to a different header file, please
+
+namespace rscn
+{
+	struct EntityList
+	{
+		unsigned long num;
+		IEntity *list[MAX_CLONES];
+
+		void clear(); 
+	}; // entitylist should act a lot like a queue, except if anything in the middle is taken out, the last item will fill in the vacant space
+	// prevent shit from getting wonky
+
+	class SceneTilt : public scn::IScene
+	{
+		public:
+		Vector3 cam_pos;
+		Vector3 cam_rot; // radians
+
+		IEntity *player;
+		EntityList clone_list;
+		EntityList enemy_list;
+	};
+}
 
 /*
 	A word of advice to Kirby GCN fans,
@@ -32,11 +93,12 @@ typedef struct SceneTilt_s
 	a warning.
 	
 	Kirby GCN was very obviously made with Air Ride and Melee's engine, meaning it used collision with polygons instead of RtDL's tile-based collision.
-	Collision is a HEADACHE. I spent approximately a year or two learning two types of polygon collision (side note, you should have a solid linear algebra base when working with 3d),
-	and reverse-engineering/hacking is another headache.
+	Collision is a HEADACHE. I spent approximately a year or two learning two types of polygon collision 
+	(side note, you should have a solid linear algebra base when working with 3d)
+	and reverse-engineering/hacking is another headache. One I also spent a whole ass year on. I lost 3-4 years of my life on Kirby GCN,
+	and this is the culmination of those efforts. A shitty tech demo made for April Fools.
 
-	I'm warning you right now, DON'T DO THIS. I'm doing it because I'm a masochist and I love experimenting with stuff like this, but you're gonna have to deal with two completely different
-	forms of bullshit all at once.
-
-	Make a standalone Kirby GCN recreation, please. You have two different decompilations to choose from. Don't do it.
+	Please just make a standalone Kirby GCN recreation. Either one on Gamecube or one on Unity if you're a cheap bastard.
+	Also? STOP PUTTING ANIME OR ORIGINAL ELEMENTS INTO YOUR GAME. I wanna see exactly what I saw in the trailers, nothing more, nothing less! Take some inspiration
+	from The Preservation Project and just do what you've seen. 
 */
---
+++ include/game/rscn.h
@@ -0,0 +1,99 @@
+/*
+phase 1 (late dec - mid january):
+have camera object be behind player obj at all times ()
+control player object with d-pad (decompile input? use libogc or wii sdk functions?)
+	later on use wii sdk or libogc functions, need to get wiimote's accelerometer for motion controls
+	have player follow basic laws of physics
+		inertia, gravity and friction
+
+have player object include a model struct (either g3d::Model or g3d::StdModel for animation functions)
+
+phase 2 (mid january - mid february):
+implement collision (wip)
+implement items and enemies
+recruit a dude or two. dunno what for, level coll or something? level design? 
+
+phase 3 (mid-late february):
+the octree problem
+	figure out how kcl blocks work
+	if not enough, write a script to generate your own octree data
+		each octree block should have an int for the parent node
+		each octree block should have 4-8 ground tris and 4-8 wall tris
+
+	figure out if you need enemies and entities to be in octree or if you want to micromanage them via screen borders
+
+get levels modelled out
+
+phase 4 (all of march):
+hurry hurry hurryyy!!!! implement EVERYTHING GET EVERYTHING GOING MUSIC ITEMS ETC
+WE NEED ATLEAST ONE OR TWO LEVELS FOR APRIL FOOLS
+
+*/
+
+namespace rcoll
+{
+	class Interval {
+		public:
+		static bool intersects(float aa, float ab, float ba, float bb); 
+	};
+
+	// interface used for collision 
+	class CollShape
+	{
+		public:
+		// members
+		hel::math::Vector3& position; // here's the deal, you supply the vector, 
+
+		// structs & classes
+		struct CollPointList {
+			// it's not really my problem if you're idiotic enough to modify the members of a CollPointList object
+			int num_points;
+			hel::Math::Vector3 points[15]; //magic number ftw
+		};
+		
+		struct CollisionInfo {
+			bool collided;
+			hel::math::Vector3 MTV;
+			// idk what else to add i didn't think i'd get this far
+		};
+
+		// virtual functions
+		virtual CollPointList getPoints(const CollShape& other); 
+		virtual CollPointList getNormals(const CollShape& other); // a normal array (should be 30 at the very most but functionally we'll only need like 6)
+
+		// normal functions
+		CollisionInfo checkCollision(const CollShape& other); // get minimum translation vector and whatnot
+	};
+
+	class Sphere : public CollShape
+	{
+		public:
+		Sphere(float radius, const Vector3& position);
+	};
+}
+// i'm making this way more complex than it needs to be, but such is the life of a c++ dev. i need simplicity
+
+namespace rscn { // roll scn
+	class Entity {
+		public:
+		virtual void update();
+		virtual void draw();	
+		virtual void endScene(); // tells all entities present to kill themselves right before the list is cleared.
+	};
+	
+	class Player : public Entity {
+		private:
+		g3d::Model* player_model;
+		rcoll::Sphere coll_sphere; // either a collision sphere or a collision aabb cube
+
+		hel::math::Vector3 position; // 
+		hel::math::Vector3 velocity; // add to position at the end of each frame.
+
+		public:
+		~Player();
+
+		// organize game logic here
+		override void update();
+		override void draw(); 
+	};
+}
