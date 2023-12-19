1e7c3593d365d624e2a38a38ec8364aeff9324e4
spookian <ianbow111@gmail.com>
2023-12-19 12:47:53 -0500

    some more setup

--- include/math/Matrix34.h
+++ include/math/Matrix34.h
@@ -21,6 +21,6 @@ namespace hel::math
 		static Matrix34 CreateLookAt(const Vector3& cam_pos, const Vector3& up, const Vector3& target);
 
 		Matrix34 operator=(const Matrix34& other);
-		Matrix34 operator*(const Matrix34& other);
+		Matrix34 operator*(const Matrix34& other) const;
 	}; // add operators later
 }
--- include/math/Vector3.h
+++ include/math/Vector3.h
@@ -2,12 +2,19 @@ namespace hel::math
 {
 	struct Vector3
 	{
+		Vector3();
+		Vector3(float _x, float _y, float _z); //0x800bdad0 - nw4r::math::VEC3 constructor
+
 		float x, y, z;
 
 		float dot(const Vector3& other) const;
+		float cos(const Vector3& other) const;
+		float length() const;
 		Vector3 cross(const Vector3& other) const;
+		void normalize();
 
 		void operator=(const Vector3& other);
+		void operator+=(const Vector3& other);
 		Vector3 operator+(const Vector3& other) const;
 		Vector3 operator-(const Vector3& other) const;
 		Vector3 operator-() const;
